829c49b41b340048ec2ac3b197338621acc6bae6
Chris Lockhart <chris.lockhart@scratchpay.com>
2020-05-04 18:15:40 -0700

    Adding framework to __init__

--- izzy/__init__.py
+++ izzy/__init__.py
@@ -1,9 +1,13 @@
 
+from . import datasets
+from . import framework
+
 from .classification import *
 from .datasets import *
 from .defaults import *
 from .eda import *
 from .features import *
+from .framework import *
 from .io import *
 from .misc import *
 from .regression import *
@@ -11,9 +15,7 @@ from .tests import *
 from .version import __version__
 from .viz import *
 
-from . import datasets
-
-
 # TODO pandas.modin for speedup? Make this an option
 __all__ = []
 __all__.extend(datasets.__all__)
+__all__.extend(framework.__all__)
