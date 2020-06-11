6d2bdc005b2487c94c4be0cd7dcd031e9620ff20
doclockh <chris@lockhartlab.org>
2020-06-10 17:48:25 -0700

    Added legend to plot

--- izzy/viz/viz.py
+++ izzy/viz/viz.py
@@ -16,7 +16,7 @@ from typelike import ArrayLike
 
 
 # Plot
-def plot(x, y=None, xlab='', ylab='', geom='line', output='auto', **kwargs):
+def plot(x, y=None, xlab='', ylab='', geom=('line', 'point'), output='auto', **kwargs):
     """
     https://matplotlib.org/tutorials/colors/colormaps.html
     https://github.com/rstudio/cheatsheets/blob/master/data-visualization-2.1.pdf
@@ -37,27 +37,30 @@ def plot(x, y=None, xlab='', ylab='', geom='line', output='auto', **kwargs):
 
     output = str(output).lower()
 
-    # If x is a DataFrame, split into columns
+    # If x is a DataFrame, x is the index and y are columns
     if isinstance(x, pd.DataFrame):
         df = x
-        x = df.index.values
-        y = df.values
+        x = df.index.names
+        y = df.columns
 
-    # y must be ArrayLike
-    if not isinstance(y, ArrayLike):
-        raise AttributeError('y must be ArrayLike')
-
-    # Dump everything into a DataFrame
-    df = pd.DataFrame({'x': x})
-    if not isinstance(y[0], ArrayLike):
-        df['y'] = y
-        y_labels = ['y']
+    # Otherwise, construct a DataFrame
     else:
-        y_labels = []
-        for i, y_i in enumerate(y):
-            y_label = 'y' + str(i)
-            df[y_label] = y_i
-            y_labels.append(y_label)
+        # y must be ArrayLike
+        if not isinstance(y, ArrayLike):
+            raise AttributeError('y must be ArrayLike')
+
+        # Dump everything into a DataFrame
+        df = pd.DataFrame({'x': x}).set_index('x')
+        if not isinstance(y[0], ArrayLike):
+            df['y'] = y
+        else:
+            for i, y_i in enumerate(y):
+                df['y' + str(i)] = y_i
+
+    # Name df.index.name as x for convenience
+    x = df.index.name
+    y = df.columns
+    df = df.reset_index().melt(id_vars=x)
 
     # Make geom an array if it's not one already; convert all elements to lowercase
     if not isinstance(geom, ArrayLike):
@@ -66,15 +69,15 @@ def plot(x, y=None, xlab='', ylab='', geom='line', output='auto', **kwargs):
         geom[i] = geom_i.lower()
 
     # Start building the figure
-    fig = p9.ggplot(df)
+    fig = p9.ggplot(df, p9.aes(x=x, y='value', color='variable', group='variable'))
     if 'line' in geom:
-        for i, y_label in enumerate(y_labels):
-            fig += p9.geom_line(p9.aes('x', y_label, group=1), color=p9.scale_color_cmap('Set1').palette(i))
+        fig += p9.geom_line()
     if 'point' in geom:
-        for i, y_label in enumerate(y_labels):
-            fig += p9.geom_point(p9.aes('x', y_label, group=1), color=p9.scale_color_cmap('Set1').palette(i))
+        fig += p9.geom_point()
     fig += p9.labs(x=xlab, y=ylab)
-    fig += p9.theme(axis_text_x=p9.element_text(rotation=45))
+    fig += p9.theme(axis_text_x=p9.element_text(rotation=45), legend_key=p9.element_blank())
+    if len(y) > 1:
+        fig += p9.scale_color_manual(name=' ', values=[p9.scale_color_cmap('Set1').palette(i) for i in range(len(y))])
 
     # Return
     if output in ['auto', 'ipython'] and get_ipython():
